aa9b0ce2039b243e3a1a6205844f1b4553988716
Vallen Dsouza <vallen2608@gmail.com>
2025-08-02 23:06:25 +0530

    day9 completed

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,10 +1,42 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
+import { useState } from "react"
+import toast from "react-hot-toast"
+
 const Signup = () => {
+
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [accountType, setaccountType] = useState("");
+
+  const handleSignup = async(e) => {
+    e.preventDefault();
+    try{
+      const res = await axios.post(import.meta.env.VITE_API_URL+"/signup", {
+        username, email, password, accountType,
+      });
+      const data = await res.data;
+      if(data.success){
+        setUsername("")
+        setEmail("");
+        setPassword("");
+        setaccountType("");
+        toast.success(data.message);
+        navigate("/login");
+      }
+    }catch(error){
+      toast.error(error.response.data.message);
+    }
+  }
+
   return (
     <div className="sm:mt-10 mt-20 min-h-screen flex items-center justify-center w-full">
       <div className="bg-white shadow-md rounded-3xl px-5 py-6 w-full sm:w-[27vw]">
         <h1 className="text-2xl font-bold text-center mb-4">Let's Connect!</h1>
-        <form>
+        <form onSubmit = {handleSignup}>
           {/* For Username */}
           <div className="mb-4">
             <label htmlFor="username" className="block text-sm font-medium text-gray-700 mb-2">Username</label>
@@ -12,6 +44,8 @@ const Signup = () => {
               type="text" 
               name="name" id="name" 
               placeholder="Vallen328" 
+              value = {username}
+              onChange = {(e) => setUsername(e.target.value)}
               className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black"/>
           </div>
 
@@ -22,6 +56,8 @@ const Signup = () => {
               type="text" 
               name="email" id="email" 
               placeholder="vallen2608@gmail.com" 
+              value = {email}
+              onChange = {(e) => setEmail(e.target.value)}
               className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black"/>
           </div>
 
@@ -32,13 +68,15 @@ const Signup = () => {
               type="password" 
               name="password" id="password" 
               placeholder="Enter your password" 
+              value = {password}
+              onChange = {(e) => setPassword(e.target.value)}
               className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black"/>
           </div>
 
           {/* For Account Selection */}
           <div className="mb-4">
             <label htmlFor="account type" className="block text-sm font-medium text-gray-700 mb-2">Select Your Account Type</label>
-            <select className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black">
+            <select onChange = {(e) => setaccountType(e.target.value)} className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black">
               <option value="buyer">Buyer</option>
               <option value="seller">Seller</option>
             </select>
