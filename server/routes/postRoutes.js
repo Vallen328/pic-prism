7388e551bbff4b34c9fef1afbe82b62e99d9d6ba
Vallen Dsouza <vallen2608@gmail.com>
2025-08-18 17:33:18 +0530

    day 15 completed

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 const {verifyToken} = require('../middlewares/verifyToken');
-const { createPost, getAllPosts, getMyPosts } = require('../controllers/postController');
+const { createPost, getAllPosts, getMyPosts, deletePost, searchPosts, addToFavourites, removeFromFavourites, getFavourites } = require('../controllers/postController');
 
 router.post("/post/create", verifyToken, createPost);
 router.get("/post/all", getAllPosts);
 router.get("/post/myPosts", verifyToken, getMyPosts);
+router.delete("/post/delete/:id", verifyToken, deletePost);
+router.get("/post/search", searchPosts);
+router.post("/post/addToFavourites/:postId", verifyToken, addToFavourites);
+router.post("/post/removeFromFavourites/:postId", verifyToken, removeFromFavourites);
+router.get("/post/favourites", verifyToken, getFavourites);
 
 module.exports = router;
